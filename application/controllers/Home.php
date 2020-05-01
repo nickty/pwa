0112d8bf063ef32eb3de63e8dd491b4d41cbcfa5
Mizanur Nickty PC <nickty.86@gmail.com>
2020-05-01 17:22:20 +0600

    php mailer

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -15,48 +15,53 @@ class Home extends CI_Controller {
 	} 
 
 
-	public function email()
-	{
-
-//Load email library
-		$this->load->library('encrypt');
-		$this->load->library('email');
-
-//SMTP & mail configuration
-		$config = array(
-			'protocol'  => 'smtp',
-			'smtp_host' => 'ssl://smtp.googlemail.com',
-			'smtp_port' => 465,
-			'smtp_user' => 'rangpurdev@gmail.com',
-			'smtp_pass' => 'Nick124578',
-			'mailtype'  => 'html',
-			'charset'   => 'utf-8'
-		);
-		$this->email->initialize($config);
-		$this->email->set_mailtype("html");
-		$this->email->set_newline("\r\n");
-
-//Email content
-		$htmlContent = '<h1>Sending email via SMTP server</h1>';
-		$htmlContent .= '<p>This email has sent via SMTP server from CodeIgniter application.</p>';
-
-		$this->email->to('nickty.86@gmail.com');
-		$this->email->from('rangpurdev@gmail.com','MyWebsite');
-		$this->email->subject('How to send email via SMTP server in CodeIgniter');
-		$this->email->message($htmlContent);
-
-//Send email
-		$sss = $this->email->send();
-
-		 if (!$sss) {
-            // Raise error message
-            show_error($this->email->print_debugger());
-        } else {
-            // Show success notification or other things here
-            echo 'Success to send email';
+	function send(){
+        // Load PHPMailer library
+        $this->load->library('phpmailer_lib');
+        
+        // PHPMailer object
+        $mail = $this->phpmailer_lib->load();
+        
+        // SMTP configuration
+        $mail->isSMTP();
+        $mail->Host     = 'smtp.example.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'rangpurdev@gmail.com';
+        $mail->Password = 'Nick124578';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port     = 465;
+        
+        $mail->setFrom('info@example.com', 'CodexWorld');
+        $mail->addReplyTo('info@example.com', 'CodexWorld');
+        
+        // Add a recipient
+        $mail->addAddress('nickty.86@gmail.com');
+        
+        // Add cc or bcc 
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
+        
+        // Email subject
+        $mail->Subject = 'Send Email via SMTP using PHPMailer in CodeIgniter';
+        
+        // Set email format to HTML
+        $mail->isHTML(true);
+        
+        // Email body content
+        $mailContent = "<h1>Send HTML Email using SMTP in CodeIgniter</h1>
+            <p>This is a test email sending using SMTP mail server with PHPMailer.</p>";
+        $mail->Body = $mailContent;
+        
+        // Send email
+        if(!$mail->send()){
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        }else{
+            echo 'Message has been sent';
         }
+    }
 
-	}
+	
 
 	public function index()
 	{
