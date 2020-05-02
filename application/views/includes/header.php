1a5246addb95b44c74b87e76ddff2d1d22cb2d3d
Mizanur Nickty PC <nickty.86@gmail.com>
2020-05-02 10:52:33 +0600

    favicon added

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -12,6 +12,24 @@
 
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+  <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url('images/favicons/apple-icon-57x57.png'); ?>">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?= base_url('images/favicons/apple-icon-60x60.png'); ?>">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url('images/favicons/apple-icon-60x60.png'); ?>/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('images/favicons/apple-icon-76x76.png'); ?>">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url('images/favicons/apple-icon-114x114.png'); ?>">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url('images/favicons/apple-icon-120x120.png'); ?>">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url('images/favicons/apple-icon-144x144.png'); ?>">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?= base_url('images/favicons/apple-icon-152x152.png'); ?>">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('images/favicons/apple-icon-180x180.png'); ?>">
+  <link rel="icon" type="image/png" sizes="192x192"  href="<?= base_url('images/favicons/android-icon-192x192.png'); ?>">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('images/favicons/favicon-32x32.png'); ?>">
+  <link rel="icon" type="image/png" sizes="96x96" href="<?= base_url('images/favicons/favicon-96x96.png'); ?>">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('images/favicons/favicon-16x16.png'); ?>">
+  <link rel="manifest" href="images/favicons/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="<?= base_url('images/favicons/ms-icon-144x144.png'); ?>">
+  <meta name="theme-color" content="#ffffff">
 </head>
 
 <body class="#eceff1 blue-grey lighten-5">
@@ -68,31 +86,31 @@
       <a href="#email"><span class="white-text email">Don't miss any lead!</span></a>
     </div></li>
     <li><a class="subheader">Core Services</a></li>
-      <ul>
-        <li><a href="<?= base_url('home/pwafromstratch/'); ?>">PWA From Scratch</a></li>
-        <li class="divider"></li>
-        <li><a href="<?= base_url('home/migration_to_pwa/') ?>">Migration To PWA</a></li>
-        <li class="divider"></li>
-        <li><a href="<?= base_url('home/pwa_optimize/') ?>">PWA Optimization</a></li>
-        <li class="divider"></li>
-        <li><a href="<?= base_url('home/pwa_seo/') ?>">PWA SEO</a></li>
-        <li class="divider"></li>
-        <li><a href="<?= base_url('home/pwa_maintenance/') ?>">PWA Maintenance</a></li>
-      </ul>
-    </li>
-   
-    <li><div class="divider"></div></li>
-    <li><a href="<?= base_url('home/work/') ?>">Work</a></li>
-    <li><div class="divider"></div></li>
-    <li><a class="subheader">Other Services</a></li>
     <ul>
-      <li><a href="<?= base_url('home/web_design/') ?>">Website Design</a></li>
+      <li><a href="<?= base_url('home/pwafromstratch/'); ?>">PWA From Scratch</a></li>
       <li class="divider"></li>
-      <li><a href="<?= base_url('home/web_development/') ?>">Website Development</a></li>
+      <li><a href="<?= base_url('home/migration_to_pwa/') ?>">Migration To PWA</a></li>
       <li class="divider"></li>
-      <li><a href="<?= base_url('home/soft_development/') ?>">Software Development</a></li>
+      <li><a href="<?= base_url('home/pwa_optimize/') ?>">PWA Optimization</a></li>
       <li class="divider"></li>
-      <li><a href="<?= base_url('home/graphic_design') ?>">Graphic Design (Only4web)</a></li>
+      <li><a href="<?= base_url('home/pwa_seo/') ?>">PWA SEO</a></li>
+      <li class="divider"></li>
+      <li><a href="<?= base_url('home/pwa_maintenance/') ?>">PWA Maintenance</a></li>
     </ul>
-    <li><a class="btn waves-effect" href="<?= base_url('home/contact'); ?>">Contact</a></li>
+  </li>
+
+  <li><div class="divider"></div></li>
+  <li><a href="<?= base_url('home/work/') ?>">Work</a></li>
+  <li><div class="divider"></div></li>
+  <li><a class="subheader">Other Services</a></li>
+  <ul>
+    <li><a href="<?= base_url('home/web_design/') ?>">Website Design</a></li>
+    <li class="divider"></li>
+    <li><a href="<?= base_url('home/web_development/') ?>">Website Development</a></li>
+    <li class="divider"></li>
+    <li><a href="<?= base_url('home/soft_development/') ?>">Software Development</a></li>
+    <li class="divider"></li>
+    <li><a href="<?= base_url('home/graphic_design') ?>">Graphic Design (Only4web)</a></li>
   </ul>
+  <li><a class="btn waves-effect" href="<?= base_url('home/contact'); ?>">Contact</a></li>
+</ul>
