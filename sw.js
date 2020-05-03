d21b3b855ef8c1ebd0b6a46181f30fecec659e8a
Mizanur Nickty PC <nickty.86@gmail.com>
2020-05-03 12:21:48 +0600

    service worker basic complited

--- sw.js
+++ sw.js
@@ -8,3 +8,9 @@ self.addEventListener('activate', function(event)
 	console.log('[Service Worker] Activating Service Worker ...', event);
 	return self.clients.claim(); 
 });
+
+self.addEventListener('fetch', function(event)
+{
+	console.log('[Service Worker] Fetching Service Worker ...', event);
+	event.respondWith(fetch(event.request));  
+});
